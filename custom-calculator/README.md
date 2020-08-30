340b1041f2583fecfa889d2b601ce2676cd804d3
alisherbegmatov <60242355+alisherbegmatov@users.noreply.github.com>
2020-08-29 23:05:04 -0700

    Add files via upload

---
+++ custom-calculator/README.md
@@ -0,0 +1,11 @@
+# Custom Calculator
+
+### This is a Tip Calculator. This calculator helps users to calculate tip amount after tax based on bill amount, and tip percentage rate.
+
+# How It Works
+
+1. The calculator will ask the user to enter the bill amount (after tax), After entering amount, the user needs to press Enter.
+2. The calculator will ask the user to enter tip percentage rate, after entering the tip percentage rate, the user needs to press Enter.
+3. The calculator will give the user total amount based on the bill amount and the tip percentage rate that the user has entered in previous steps.
+
+# Alisher Begmatov
---
+++ custom-calculator/custom-calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to tip calculator!")
+bill = input("Please enter your bill amount: ")
+tip = input("Please enter your tip percentage rate: ")
+
+bill = int(bill)
+tip = int(tip)
+
+print("Your total is: ", bill + bill * tip / 100)
+
+
+# Custom Calculator
+
+# This is a Tip Calculator. This calculator helps users to calculate tip amount after tax based on bill amount, and tip percentage rate.
+
+# How It Works
+
+# 1. The calculator will ask the user to enter the bill amount (after tax), After entering amount, the user needs to press Enter.
+# 2. The calculator will ask the user to enter tip percentage rate, after entering the tip percentage rate, the user needs to press Enter.
+# 3. The calculator will give the user total amount based on the bill amount and the tip percentage rate that the user has entered in previous steps.
+
+# Alisher Begmatov
